d20b822c61d257ccf178d9b714a2c75397f2c130
lamantur <Lamantur2017@gmail.com>
2022-12-03 13:35:30 +0300

    задача *

--- star/Program.cs
+++ star/Program.cs
@@ -22,7 +22,7 @@ int Prompt(string message)
     return Convert.ToInt32(Console.ReadLine());
 }
 
-/*int SecondMax(int[] array)
+int SecondMax(int[] array)
 {
     int Max = 0;
     int Second = 0;
@@ -44,8 +44,8 @@ int Prompt(string message)
     }
     return Second;
 }
-*/
+
 
 int[] arr = GenerateArray(Prompt("Введите количество элементов массива"), Prompt("Введите минимальный порог случайных значений"), Prompt("Введите максимальный порог случайных значений"));
 PrintArray(arr);
-//System.Console.WriteLine("Второй максимальный элемент: " + SecondMax(arr));
+System.Console.WriteLine("Второй максимальный элемент: " + SecondMax(arr));
