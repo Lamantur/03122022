c3c5e6e7b7dc939262c2227b492b2e2994844884
lamantur <Lamantur2017@gmail.com>
2022-12-03 13:38:06 +0300

    zd

--- 3/Program.cs
+++ 3/Program.cs
@@ -22,30 +22,6 @@ int Prompt(string message)
     return Convert.ToInt32(Console.ReadLine());
 }
 
-/*int SecondMax(int[] array)
-{
-    int Max = 0;
-    int Second = 0;
-
-    for (int j = 0; j < array.Length; j++)
-    {
-        if (array[j] < Max)
-        {
-            if (array[j] > Second)
-            {
-                Second = array[j];
-            }
-        }
-        else
-        {
-            Second = Max;
-            Max = array[j];
-        }
-    }
-    return Second;
-}
-*/
-
 int[] arr = GenerateArray(Prompt("Введите количество элементов массива"), Prompt("Введите минимальный порог случайных значений"), Prompt("Введите максимальный порог случайных значений"));
 PrintArray(arr);
-//System.Console.WriteLine("Второй максимальный элемент: " + SecondMax(arr));
+
